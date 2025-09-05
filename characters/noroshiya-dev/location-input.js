b5bac420ab84ae3d0488f3e87366f1ba229632e6
REMriaTV <remuria@gmail.com>
2025-09-05 15:43:13 +0900

    地図を最初から埋め込み表示、石マップと同じシンプルなデザインに統一

--- characters/noroshiya-dev/location-input.js
+++ characters/noroshiya-dev/location-input.js
@@ -76,9 +76,11 @@ function initializeLocationMap() {
     // 地図を初期化
     locationMap = L.map('location-map').setView([currentLat, currentLng], 15);
     
-    // OpenStreetMapタイル
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
+    // CartoDBの明るいタイルレイヤー（石マップと同じスタイル）
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        subdomains: 'abcd',
+        maxZoom: 20
     }).addTo(locationMap);
     
     // クリックイベント
